9f573752e4b64140801f03dbf908f210a90b7194
Ingrid <maevachekma@gmail.com>
2026-06-13 14:55:03 +0200

    Chore: Corrected the addition of environemnt variabled to bashrc

--- docker_to_run/setup_app.sh
+++ docker_to_run/setup_app.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+cd
+cp /etc/skel/.bashrc .
+
+cat << 'EOF' >> ~/.bashrc
+ "\. "$HOME/.nvm/nvm.sh"
+ export SECRET_KEY=mySecretKey
+export DATABASE_USERNAME=$POSTGRES_USER
+export DATABASE_PASSWORD=$POSTGRES_PASSWORD
+export DATABASE_NAME=$POSTGRES_DB
+
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion"
+
+EOF
 
 git clone https://github.com/maeri18/UniResidences.git ~/UniResidences
 
